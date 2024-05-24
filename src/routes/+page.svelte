401f3116be968ce2cd5e53d64f29b5ecedc96301
sha5b <ned.tabulov@gmail.com>
2024-05-24 12:11:14 +0200

    First Commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    // You can add any necessary script logic here
+  </script>
+  
+  <style>
+    @keyframes rotate {
+      0% {
+        transform: rotate(0deg) scale(1);
+        color: red;
+      }
+      25% {
+        transform: rotate(90deg) scale(1.2);
+        color: blue;
+      }
+      50% {
+        transform: rotate(180deg) scale(1);
+        color: green;
+      }
+      75% {
+        transform: rotate(270deg) scale(0.8);
+        color: yellow;
+      }
+      100% {
+        transform: rotate(360deg) scale(1);
+        color: red;
+      }
+    }
+  
+    .rotating-text {
+      display: inline-block;
+      font-size: 2em;
+      animation: rotate 4s infinite;
+    }
+  
+    .container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      text-align: center;
+    }
+  </style>
+  
+  <div class="container">
+    <div class="rotating-text">Mirai Systems</div>
+  </div>
+  
